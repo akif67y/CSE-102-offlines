b7519963c974728976b710420c6ad8708750b591
Ahnaf Akif <akif289789@gmail.com>
2024-11-28 18:39:59 +0600

    formatted

--- frequency.c
+++ frequency.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* takearray(){
-    int *temp = (int *) malloc(10001 * sizeof(int));
+int *takearray()
+{
+    int *temp = (int *)malloc(10001 * sizeof(int));
     return temp;
 }
-int main(){
+int main()
+{
     int n;
     scanf("%d", &n);
     int *freq;
     freq = takearray();
-    for(int i = 0; i < 10001; i++){
+    for (int i = 0; i < 10001; i++)
+    {
         *(freq + i) = 0;
     }
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int t;
         scanf("%d", &t);
-        *(freq + t) =  *(freq + t) + 1;
+        *(freq + t) = *(freq + t) + 1;
     }
     int max = 0;
-    for(int i = 0; i < 10001; i++ ){
-        if(*(freq + i) > max){
-            max = *(freq+i);
+    for (int i = 0; i < 10001; i++)
+    {
+        if (*(freq + i) > max)
+        {
+            max = *(freq + i);
         }
     }
-      for(int i = 0; i < 10001; i++ ){
-        if(*(freq + i) == max){
+    for (int i = 0; i < 10001; i++)
+    {
+        if (*(freq + i) == max)
+        {
             printf("%d ", i);
         }
     }
     free(freq);
-
 }
--- pointers-1.c
+++ pointers-1.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-char * take_string (int n){
-    char *temp = malloc(n * sizeof(char));
+char *take_string(int n)
+{
+    char *temp =(char *) malloc(n * sizeof(char));
     return temp;
 }
 
-void reverseWords(char *a, char *t){
+void reverseWords(char *a, char *t)
+{
     int n = strlen(a);
 
-    int word = 0, k = 0, start =  n - 1, end = n - 1;
-    for(int i = n - 1; i >= -1 ; i--){
-        if(a[i] == ' ' || i == -1){
-            if(start != end){
-                if(word != 0){
-                    *(t+k) =' ';
+    int word = 0, k = 0, start = n - 1, end = n - 1;
+    for (int i = n - 1; i >= -1; i--)
+    {
+        if (a[i] == ' ' || i == -1)
+        {
+            if (start != end)
+            {
+                if (word != 0)
+                {
+                    *(t + k) = ' ';
                     k++;
                 }
-                word++; 
-                for(int j = 1; j <= end - start; j++){
-                    *(t+k) = *(a + start + j);
+                word++;
+                for (int j = 1; j <= end - start; j++)
+                {
+                    *(t + k) = *(a + start + j);
                     k++;
                 }
-                
             }
             end = start - 1;
             start = end;
-
         }
-        else {
+        else
+        {
             start--;
         }
     }
-    *(t + k) ='\0';
-
+    *(t + k) = '\0';
 }
-int main(){
+int main()
+{
     char *a, *t;
     a = take_string(1000);
     t = take_string(1000);
@@ -44,5 +50,4 @@ int main(){
     free(a);
     printf("%s", t);
     free(t);
-
 }
